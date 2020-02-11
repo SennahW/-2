6a8f89155ed944c5cd5905603b18bda63b380cf7
SennahW <johannes.wetterlund@gmail.com>
2020-02-11 08:58:35 +0100

    Revert "fdfd"
    This reverts commit 9778907058411d63b5a72c10ce1ca2dd2cdebe84.

---
+++ 未煮熟/Assets/Resources/Scripts/Game1/DragHandler.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{ 
+    public static GameObject myItemBegingDragged;
+    Vector3 myStartPosition;
+    Transform myStartParent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        myItemBegingDragged = gameObject;
+        myStartPosition = transform.position;
+        myStartParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        myItemBegingDragged = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent == myStartParent)
+        {
+            transform.position = myStartPosition;
+        }
+    }
+
+    public void SpawnOnion()
+    {
+        GameObject.FindGameObjectWithTag("Cuttingboard").GetComponent<Slots>().SpawnChoppenGeenOnion();
+        Destroy(this.gameObject);
+    }
+
+    public void SpawnLemon()
+    {
+        GameObject.FindGameObjectWithTag("Cuttingboard").GetComponent<Slots>().SpawnChoppedLemon();
+        Destroy(this.gameObject);
+    }
+
+    public void SpawnCookedEgg()
+    {
+        Debug.Log("Spawn1");
+        GameObject.FindGameObjectWithTag("FryingpanOne").GetComponent<Slots>().SpawnCookedEgg();
+        Destroy(this.gameObject);
+    }
+
+    public void SpawnCookedEggTwo()
+    {
+        Debug.Log("Spawn2");
+        GameObject.FindGameObjectWithTag("FryingpanTwo").GetComponent<Slots>().SpawnCookedEgg();
+        Destroy(this.gameObject);
+    }
+
+    public void SpawnCookedRiceOne()
+    {
+        GameObject.FindGameObjectWithTag("PotOne").GetComponent<Slots>().SpawnCookedRice();
+        Destroy(this.gameObject);
+    }
+
+    public void SpawnCookedRiceTwo()
+    {
+        GameObject.FindGameObjectWithTag("PotTwo").GetComponent<Slots>().SpawnCookedRice();
+        Destroy(this.gameObject);
+    }
+}
---
+++ 未煮熟/Assets/Resources/Scripts/Game1/Game0ver.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Game0ver : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject myGameOverUI;
+
+    void Update()
+    {
+      // if(//om spelaren förlorar)
+        {
+
+        }
+    }
+
+    public void PausedGame()
+    {
+        myGameOverUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
